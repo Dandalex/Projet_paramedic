dc309f601c9baad9f7eecbe479692da038371ff0
alexdandrea <alexdandrea@192.168.1.27>
2023-10-22 18:14:32 -0400

    Ajout conversion lbs a kg

--- ParamedicQuebec/src/fonctions/Poids.java
+++ ParamedicQuebec/src/fonctions/Poids.java
@@ -1,10 +1,37 @@
 package fonctions;
+import java.util.Scanner;
+
+
 
 public class Poids {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	System.out.println("Entrer le poids");
+	Scanner scan = new Scanner(System.in);
+	double poids = scan.nextDouble();
+	System.out.println("terminer votre ligne avec k pour Kilogrammes, ou l pour livres");
+	char unite = scan.next().charAt(0);
+	
+		if (unite=='l'||unite=='L') {
+			poids= lbsToKg (poids);
+			unite = 'k';
+		}		
+		System.out.println();
+		System.out.printf("Le poids est %.2f ",poids);
+		System.out.print(unite);
+		
 	}
+
+
+	public static double lbsToKg (double x) {
+		// TODO Auto-generated method stub
+		x=x/2.2;
+		return x;
+	}
+	public static double kgToLbs (double x) {
+		// TODO Auto-generated method stub
+		x=x*2.2;
+		return x;
+	}
+	
 
 }
